26831bcaf31ed4f6a317ed683e254988791ad663
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2018-03-22 15:26:16 +0700

    coruse reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import authReducers from './authReducers';
 import userReducers from './userReducers';
 import periodReducers from './periodReducers';
-import courseReducers from './courseReducers';
+import courseReducers from './courseReducer';
 import operationRoomReducers from './operationRoomReducers';
 import { reducer as formReducer } from 'redux-form'
 const rootReducers = combineReducers({
