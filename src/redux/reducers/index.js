1f0991396c0260a1a98f03d23322195144d8f78a
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2018-03-22 14:20:53 +0700

    s

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import authReducers from './authReducers';
 import userReducers from './userReducers';
 import periodReducers from './periodReducers';
+import courseReducers from './courseReducers';
 import { reducer as formReducer } from 'redux-form'
 const rootReducers = combineReducers({
     form: formReducer,
