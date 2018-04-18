2fae7639b251509270b2995fab4350116441136a
chayakornwc <chayakorn.lpru@gmail.com>
2018-04-18 16:05:27 +0700

    d
    d

--- server/controllers/Attender.js
+++ server/controllers/Attender.js
@@ -7,13 +7,14 @@ exports.findById = (req,res,next) => {
     var id = parseInt(req.params.id);
     req.getConnection((err, connection)=>{
         if(err) return next(err);
-        var sql = "SELECT r.prefix, r.firstname, r.last_name, gender, r.major, r.affiliation, r.company, r.username, period.*, course.*, count(course_order.order_id) as period_quantity FROM period LEFT JOIN course ON period.course_id = course.course_id"
+        var sql = "SELECT r.prefix, r.firstname, r.last_name, gender, r.major, r.affiliation, r.company, r.username, period.*, course.* FROM period LEFT JOIN course ON period.course_id = course.course_id"
         +" LEFT JOIN course_order ON course_order.course_id = course.course_id where period.per_id=?"; 
-        connection.query(sql,[id],function(err, results){
-        if(err) return next(err);
-        res.send(results[0])
+        console.log(sql);
+    //     connection.query(sql,[id],function(err, results){
+    //     if(err) return next(err);
+    //     res.send(results[0])
          
-    })  
-   
+    // })  
+    // SELECT r.prefix, r.first_name, r.last_name, gender, r.major, r.affiliation, r.company, r.username, period.*, course.* FROM period LEFT JOIN course ON period.course_id = course.course_id LEFT JOIN course_order ON course_order.course_id = course.course_id LEFT JOIN registration r ON course_order.registration_id = r.id where period.per_id=15
     })  
 }
