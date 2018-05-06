b5f7e63edef4c12a172bed9b505de22fa667e07b
Chayakorn Kaewwong <whitecat.chayakorn@gmail.com>
2018-05-06 23:28:14 +0700

    :construction:

--- server/controllers/Examination.js
+++ server/controllers/Examination.js
@@ -3,7 +3,7 @@ const config = require('../config');
 exports.findById = (req,res,next)=>{
     var course_id = parseInt(req.params.course_id);
     var exam_id = parseInt(req.params.exam_id);
-    req.getConntection((err, connection)=>{
+    req.getConnection((err, connection)=>{
         if(err) return next(err);
         connection.query("SELECT * FROM course_exam WHERE course_id =? AND exam_id=? ",[course_id, exam_id], (err, results)=>{
             if(err) return next(err);
@@ -21,11 +21,20 @@ exports.create = (req,res,next)=>{
         answer4:req.body.answer4,
         answer_real:rea.body.answer_real
     }
-    req.getConntection((err, connection)=>{
+    req.getConnection((err, connection)=>{
         if(err) return next(err);
         connection.query("INSERT INTO course_exam set ? WHERE course_id=?", [data, course_id], (err, results)=>{
             if(err) return next(err);
             res.send({status:200, message:'เพิ่มข้อมูลเรียบร้อยแล้ว'});
         });
     })
+}
+exports.findNullexam = (req,res,next)=>{
+        req.getConnection((err, connection)=>{
+            if(err) return next(err);
+            connection.query("SELECT c.*, ex.exam_id from course c LEFT OUTER JOIN course_exam ex ON ex.course_id = c.course_id WHERE  ex.exam_id IS NULL GROUP BY c.course_id", (err, results)=>{
+                if(err) return next(err);
+                res.send(results)
+            })
+        })
 }
