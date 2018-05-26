96381e5ae92698f63d9fbea25edd2698e887f02a
chayakornwc <whitecat.chayakorn@gmail.com>
2018-05-26 21:22:12 +0700

    :hammer:  remove thunk

--- src/redux/actions/periodActions.js
+++ src/redux/actions/periodActions.js
@@ -31,44 +31,7 @@ export const getPeriod = (id)=>{
         })
     }
 }   
-// add and edit period 
-export const savePeriod = (values)=>{
-    let _id ='';    
-    let _method ='post';
-        if(values.per_id){
-            _id = values.per_id;
-            _method='put';
-        }
-        
-        return(dispatch)=>{
-            dispatch({type:'SAVE_PERIOD_PENDING'})
-            return axios({
-                method:_method,
-                url:`${BASE_URL}/period/${_id}`,
-                data:values,
-                headers: { authorization: localStorage.getItem('token') }
-            }).then(results =>{
-                dispatch({type:'SAVE_PERIOD_SUCCESS', payload:results.data})
-            }).catch(err =>{
-                dispatch({type:'SAVE_PERIOD_REJECTED', payload:err.message})
-            })
-        }
-}
-//delete period
-export const deletePeriod = (id)=>{
-    return (dispatch)=>{
-       
-        return axios({
-            method:'delete',
-            url:`${BASE_URL}/period/${id}`,
-            headers:{authorization:localStorage.getItem('token') }
-        }).then(results =>{
-            dispatch({type:'DELETE_PERIOD_SUCCESS'})
-        }).catch(err=>{
-            dispatch({type:'DELETE_PERIOD_REJECTED', payload:err.message})
-        })
-    }
-}
+
 // reset error message
 export const resetStatus = () =>{
     return(dispatch)=>{
