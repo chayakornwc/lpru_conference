e61e8006a97badc26812db5f718f6d628969ce3f
chayakornwc <whitecat.chayakorn@gmail.com>
2018-06-09 23:22:42 +0700

    :construction:

--- src/components/Examination/WizardFormSecondsPage.js
+++ src/components/Examination/WizardFormSecondsPage.js
@@ -1,6 +1,7 @@
 import React  from 'react'
 import { Field, reduxForm } from 'redux-form'
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import renderRadio from './renderRadio';
 const WizardFormSecondsPage = (props)=> {
     const {examinations,previousPage,handleSubmit} = props
     return (
@@ -13,24 +14,24 @@ const WizardFormSecondsPage = (props)=> {
             <div className="container">
                 <div className="columns" key={i}>
                   <div className="column">
-                      {ReactHtmlParser(unescape(e.question))}
+                      ข้อ{(i+1)}{'. '}{ReactHtmlParser(unescape(e.question))}
                   </div>
                 </div>
 
                 <div className="columns">
-                <div className="column">
-                  {unescape(e.answer1)}
-                </div>
-                <div className="column">
-                {unescape(e.answer1)}
-                </div>
+                  <div className="column">
+                    <Field name={e.exam_id} label={ReactHtmlParser(unescape(e.answer1))} value="1" type="radio" component={renderRadio}/>
+                  </div>
+                  <div className="column">
+                  <Field name={e.exam_id} label={ReactHtmlParser(unescape(e.answer2))} value="2" type="radio" component={renderRadio}/>
+                  </div>
               </div>
               <div className="columns">
                 <div className="column">
-                {unescape(e.answer1)}
+                <Field name={e.exam_id} label={ReactHtmlParser(unescape(e.answer3))} value="3" type="radio" component={renderRadio}/>
                 </div>
                 <div className="column">
-                {unescape(e.answer1)}
+                <Field name={e.exam_id} label={ReactHtmlParser(unescape(e.answer4))} value="4" type="radio" component={renderRadio}/>
                 </div>
               </div>
             </div>
---
+++ src/components/Examination/renderRadio.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+//renderField จะรับ props ต่างๆ ของ Field ที่ได้จาก redux-form
+const renderRadio = ({ input, label,  type, textarea, autoFocus, meta: { touched, error } }) => {
+//สำหรับรูปแบบ Field ที่เป็น TextArea
+
+//สำหรับรูปแบบ Field ที่เป็น TextBox
+
+    return (
+        
+        <div class="field is-narrow">
+            <div class="control">
+                <label class="radio">
+                <input {...input} type={type}  checked={input.checked} />
+                {' '}{label}
+                </label>
+              
+            </div>
+            {touched && error && <p className="help is-danger">{error}</p>}
+        </div>              
+                    
+                    )
+    }
+export default renderRadio;
